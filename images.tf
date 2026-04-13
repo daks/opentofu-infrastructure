2350a9db54c05308d0879de5c5922529da1082eb
Eric Veiras <eric@sietch-tabr.com>
2026-04-13 15:28:31 +0200

    update image used to the last debian 13

--- images.tf
+++ images.tf
@@ -1,6 +1,6 @@
 # Scaleway images definitions
 
 ## sys base
-data "scaleway_instance_image" "buster" {
-  name = "debian_buster"
+data "scaleway_instance_image" "debian-13" {
+  name = "debian-13-2026-04-12-1723"
 }
